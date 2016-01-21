7d8e5d7d991663b0bc23a567bee2b656c6c17986
EastskyKang <east0822@gmail.com>
2016-01-21 20:12:39 +0900

    iml file delete

--- www/nice.js
+++ www/nice.js
@@ -6,13 +6,4 @@
 
 var exec = cordova.require('cordova/exec');
 
-/**
- * PushNotification constructor.
- *
- * @param {Object} options to initiate Push Notifications.
- * @return {PushNotification} instance that can be monitored and cancelled.
- */
-
-var NicePay = function(options) {
-
-}
+var NicePay = function(options) {};
